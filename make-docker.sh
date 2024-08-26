c726bb4b7801d16595bfe6fc2420fc8a775d3196
z11i <geekdeedy@outlook.com>
2024-08-26 20:52:45 +0800

    Add clickhouse default config file

--- make-docker.sh
+++ make-docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
-cp Dockerfile ClickHouse/build-arm64/programs
+cp docker/Dockerfile ClickHouse/build-arm64/programs
+cp docker/entrypoint.sh ClickHouse/build-arm64/programs
+cp -r docker/config/* ClickHouse/build-arm64/programs
+cp -r docker/docker-entrypoint-initdb.d ClickHouse/build-arm64/programs
 cd ClickHouse/build-arm64/programs
 tar -cvJf ck.tar.xz clickhouse* --exclude=clickhouse-library-bridge --exclude=clickhouse-odbc-bridge
 docker build . -t clickhouse:tag
